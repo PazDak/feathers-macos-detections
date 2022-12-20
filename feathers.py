6b2ef5621ae06277a3f754d41cd9650d46470bf6
pazdak <kyle.pazandak@gmail.com>
2022-12-20 11:46:22 -0600

    Initial Commit

--- feathers.py
+++ feathers.py
@@ -1,13 +1,17 @@
 import json
+import time
 from subprocess import PIPE, STDOUT
 import subprocess
 import sys
+import hashlib
 try:
     import requests
 except Exception as E:
     print("You need requests run the next line: \npip install requests")
     raise Exception('You need requests run the next line: \npip install requests') from E
 
+with open("exclude.json", 'r', encoding="utf-8") as fp:
+    exclude_rules = json.loads(fp.read())
 
 def getJSonFromCommandLine(cmd):
     response = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
@@ -32,12 +36,34 @@ def getBundleId(appName):
 
 def useSystemProfiler():
     apps = getJSonFromCommandLine(cmd="system_profiler -json SPApplicationsDataType")["SPApplicationsDataType"]
+    result_apps = []
     for app in apps:
+        if app['obtained_from'] == "apple" and app['version'] != "1.0":
+            continue
+        if 'version' not in app:
+            continue
         app['bundleId'] = getBundleId(appName=app["_name"])
-    return apps
+        if app['bundleId'] not in exclude_rules['apps']['ignoreBundleIds']:
+            del_keys = ['arch_kind', 'signed_by', 'lastModified']
+            for key in del_keys:
+                if key in app:
+                    del app[key]
+            try:
+                hash_string = f"{app['_name']}{app['bundleId']}{app['version']}"
+            except:
+                print(app)
+                time.sleep(3)
+
+            app['app_hash'] = hashlib.md5(hash_string.encode()).hexdigest()
+            result_apps.append(app)
+    return result_apps
 
 def getSystemInfo():
     osVersion = getJSonFromCommandLine(cmd="system_profiler -json SPSoftwareDataType")["SPSoftwareDataType"][0]
+    del_keys = ['local_host_name', 'user_name', 'uptime', 'secure_vm', 'system_integrity', 'boot_mood', 'boot_volum']
+    for key in del_keys:
+        if key in osVersion:
+            del osVersion[key]
     return osVersion
 
 def getRunningPids(cveDetails:list)->dict:
@@ -56,16 +82,19 @@ def getRunningPids(cveDetails:list)->dict:
 
 
 if __name__ == "__main__":
+    commands = ['token', 'cisa', 'cisapastdue', 'severity', 'output', 'cve', 'splunk_token', 'splunk_host']
+
     results = {}
-    if '-SystemProfilerApps' in sys.argv:
-        results['apps'] = useSystemProfiler()
-    if '-SystemProfilerOS' in sys.argv:
-        results['os'] = getSystemInfo()
+    print(sys.argv)
+    args = {}
+    for arg in sys.argv:
+        if '-token' in arg:
+            args['token'] = arg.split("=")[1].replace("\"", "")
+    results['apps'] = useSystemProfiler()
+    results['os'] = getSystemInfo()
 
-    if '-OnlyCVE' in sys.argv:
-        url = "https://feathers.pazops.com/api/macos/system_profiler"
-        results = requests.post(url=url, data=json.dumps(results)).json()
-    if '-CheckRunning' in sys.argv:
-        results['runningCVE'] = getRunningPids(cveDetails=results['cveDetails'])
-
-    print(json.dumps(results, indent=2))
+    url = "https://feathers.pazops.com/api/macos/system_profiler"
+    results_hash = hashlib.md5(json.dumps(results, sort_keys=True).encode()).hexdigest()
+    results = requests.post(url=url, data=json.dumps(results, sort_keys=True)).json()
+    print(json.dumps(results, indent=2, sort_keys=True))
+    print(results_hash)
